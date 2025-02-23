76a0e750737f0e6fcd9da095d7132730d1e89e40
Deena Sun <deenasun@live.com>
2025-02-22 21:47:50 -0800

    [refactor] state variables, added footer with github

---
+++ src/api/gunicorn_config.py
@@ -0,0 +1,3 @@
+workers = 2
+bind = "127.0.0.1:8000"
+timeout = 120  # Increase timeout to 120 seconds
---
+++ src/app/page.js
@@ -0,0 +1,44 @@
+'use client';
+
+config();
+
+import Image from 'next/image';
+import { GameProvider } from './gameContext';
+import Game from './game';
+import { config } from 'dotenv';
+
+export default function Home() {
+    return (
+        <div className="grid grid-rows-[15px_1fr_15px] items-center justify-items-center min-h-screen p-6 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-4 row-start-2 items-center">
+                <Image
+                    src="/wordlogo.svg"
+                    alt="Guesscaso logo"
+                    width={360}
+                    height={120}
+                    priority
+                />
+                <GameProvider>
+                    <Game />
+                </GameProvider>
+            </main>
+            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex mt-2 items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/deenasun/guesscasso"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Guesscasso on Github
+        </a>
+      </footer>
+        </div>
+    );
+}
